663ce48bfd369bf33d1639402d6fd506738fdf40
Hallowdread <hallowpahe@gmail.com>
2025-01-02 10:52:01 +0100

    added split bill form

--- src/App.jsx
+++ src/App.jsx
@@ -48,11 +48,39 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendList friends={friends} />
+
         {showAddFriend && <FormAddFriend onAddNewFriend={handleAddNewFriend} />}
+
         <Button onClick={handleShowAddFriend}>
           {showAddFriend ? "Close" : "Add Friend"}
         </Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
+
+const FormSplitBill = () => {
+  return (
+    <form className="form-split-bill">
+      <h2>Split Bill With X</h2>
+
+      <label htmlFor="billValue">💰 Bill Value</label>
+      <input type="number" />
+
+      <label htmlFor="userExpense">🕴🏾 Your Expense</label>
+      <input type="number" />
+
+      <label htmlFor="friendExpense">👫 X's Expense</label>
+      <input type="number" />
+
+      <label>Who is playing the bill</label>
+      <select>
+        <option value="">You</option>
+        <option value="">X</option>
+      </select>
+
+      <Button>Add</Button>
+    </form>
+  );
+};
